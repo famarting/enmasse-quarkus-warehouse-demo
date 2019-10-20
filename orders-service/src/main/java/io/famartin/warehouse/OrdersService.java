06a8c30a91d4aba28d453eb32291987af9aa5d80
Fabian Martinez <famartin@redhat.com>
2019-10-20 12:26:33 +0200

    upgrade to quarkus 0.25.0

--- orders-service/src/main/java/io/famartin/warehouse/OrdersService.java
+++ orders-service/src/main/java/io/famartin/warehouse/OrdersService.java
@@ -8,8 +8,8 @@ import javax.inject.Inject;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
+import io.smallrye.reactive.messaging.annotations.Channel;
 import io.smallrye.reactive.messaging.annotations.Emitter;
-import io.smallrye.reactive.messaging.annotations.Stream;
 import io.vertx.core.json.JsonObject;
 
 /**
@@ -27,33 +27,33 @@ public class OrdersService {
     StocksService stocks;
 
     @Inject
-    @Stream("processed-orders")
+    @Channel("processed-orders")
     Emitter<JsonObject>  processedOrders;
 
     @Incoming("orders")
-    public CompletionStage<?> processOrder(JsonObject order) {
+    public CompletionStage<Void> processOrder(JsonObject order) {
         events.sendEvent("Processing order "+order.getString("order-id"));
-        longRunningOperation();
-        CompletionStage<JsonObject> response = stocks.requestStock(order.getString("order-id"),
-                order.getString("item-id"), order.getInteger("quantity"));
-        return response.thenAccept(result -> {
-
-            order.put("processingTimestamp", result.getString("timestamp"));
-            order.put("processedBy", EventsService.SERVICE_NAME);
-
-            if (result.containsKey("error")) {
-                order.put("error", result.getString("error"));
-                order.put("approved", false);
-            } else if (result.getBoolean("approved", false)) {
-                order.put("approved", true);
-            } else {
-                order.put("approved", false);
-                order.put("reason", result.getString("message"));
-            }
-            processedOrders.send(order);
-            events.sendEvent("Order " + order.getString("order-id") + " processed");
-
-        });
+        return stocks.requestStock(order.getString("order-id"),
+                order.getString("item-id"), order.getInteger("quantity"))
+            .thenApplyAsync(result -> {
+                longRunningOperation();
+                return result;
+            })
+            .thenAccept(result -> {
+                order.put("processingTimestamp", result.getString("timestamp"));
+                order.put("processedBy", EventsService.SERVICE_NAME);
+                if (result.containsKey("error")) {
+                    order.put("error", result.getString("error"));
+                    order.put("approved", false);
+                } else if (result.getBoolean("approved", false)) {
+                    order.put("approved", true);
+                } else {
+                    order.put("approved", false);
+                    order.put("reason", result.getString("message"));
+                }
+                processedOrders.send(order);
+                events.sendEvent("Order " + order.getString("order-id") + " processed");
+            });
     }
 
     private void longRunningOperation() {
